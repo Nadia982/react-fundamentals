c3d2e74845be479e8e239eac932552acdcdc26f6
Natalie <79932225+Nadia982@users.noreply.github.com>
2023-03-12 02:00:49 +0000

    Story 30 (First React Component) tasks completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function FirstReactComponent() {
+  return <p>I am new here!</p>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <p>You look beautiful</p>
+      <FirstReactComponent />
     </div>
   );
 }
